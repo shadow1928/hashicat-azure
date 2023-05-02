42f787292cfcaff69eb92444fac598b782646dfd
Frank Lugo <frank.lugo378@gmail.com>
2023-05-02 19:26:35 +0000

    Added network module

--- main.tf
+++ main.tf
@@ -28,6 +28,27 @@ resource "azurerm_resource_group" "myresourcegroup" {
   }
 }
 
+module "networking" {
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.example.name
+  address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+
+  subnet_service_endpoints = {
+    "subnet1" : ["Microsoft.Sql"], 
+    "subnet2" : ["Microsoft.Sql"],
+    "subnet3" : ["Microsoft.Sql"]
+  }
+
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
+
+  depends_on = [azurerm_resource_group.example]
+}
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-vnet"
   location            = azurerm_resource_group.myresourcegroup.location
---
+++ network.tf
@@ -0,0 +1,4 @@
+module "network" {
+  source  = "app.terraform.io/TFC102/network/azurerm"
+  version = "3.5.0"
+}
